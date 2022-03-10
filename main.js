433912fd8bfb745865f8a06aa290df179e649b15
JarnovdPoll <jarno.van.de.poll@outlook.com>
2022-03-10 15:20:48 +0100

    Started working on the header, added assets folder

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 class MainHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = ` 
-        <div class="header">
-  <a href="#home" class="logo">CompanyLogo</a>
-  <div class="header-right">
-    <a href="#home">Home</a>
-  </div>
-</div>
         `
     }
 }
